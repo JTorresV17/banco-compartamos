5b19142bdb0cb28c82388ce9b645ed82778666f4
jeremyjbo98 <jeremy.ballesteros@omni.pro>
2025-04-09 17:30:30 -0500

    avance bloque slider

--- blocks/banco-compartir-carousel/banco-compartir-carousel.js
+++ blocks/banco-compartir-carousel/banco-compartir-carousel.js
@@ -14,6 +14,7 @@ export default function decorate(block) {
 
   prevButton.className = 'banco-compartir-carousel-button-prev';
   nextButton.className = 'banco-compartir-carousel-button-next';
+  prevButton.disabled = true;
 
   const items = [...block.children];
 
@@ -29,5 +30,33 @@ export default function decorate(block) {
   block.appendChild(carouselContainer);
   block.appendChild(nextButton);
   block.prepend(prevButton);
+
+  let currentIndex = 0; // Mantener el índice de la imagen actual
+  const itemWidth = items[0].offsetWidth; // Suponiendo que todos los items tienen el mismo tamaño
+
+  function moveCarousel() {
+    carouselContainer.style.transform = `translateX(-${currentIndex * itemWidth}px)`;
+
+    // Deshabilitar o habilitar los botones según el índice
+    prevButton.disabled = currentIndex === 0; // Deshabilitar "Prev" en el primer slide
+    nextButton.disabled = currentIndex === items.length - 1; // Deshabilitar "Next" en el último slide
+  }
+
+  prevButton.addEventListener('click', () => {
+    if (currentIndex > 0) {
+      currentIndex--;
+      moveCarousel();
+    }
+  });
+
+  nextButton.addEventListener('click', () => {
+    if (currentIndex < items.length - 1) {
+      currentIndex++;
+      moveCarousel();
+    }
+  });
+
+  // Inicializar el carrusel al principio
+  moveCarousel();
   
 }
