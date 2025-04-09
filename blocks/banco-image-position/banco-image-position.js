d85b9be8e3f170dead034b232d11edcd8bacb71b
JTorresV17 <juan.torres@omni.pro>
2025-04-09 12:31:17 -0500

    ajustes bloque de image

--- blocks/banco-image-position/banco-image-position.js
+++ blocks/banco-image-position/banco-image-position.js
@@ -3,13 +3,10 @@ import { moveInstrumentation } from '../../scripts/scripts.js';
 export default function decorate(block) {
   block.innerHTML = '';
 
+  const { positionImage, title, description, textColor } = block.dataset;
+
   const container = document.createElement('div');
   container.classList.add('image-text-container');
-
-  const positionImage = block.dataset.positionImage;
-  const title = block.dataset.title;
-  const description = block.dataset.description;
-  const textColor = block.dataset.textColor || '#ffffff';
 
   const imageWrapper = document.createElement('div');
   imageWrapper.classList.add('image-wrapper');
@@ -22,7 +19,7 @@ export default function decorate(block) {
 
   const textWrapper = document.createElement('div');
   textWrapper.classList.add('text-wrapper');
-  textWrapper.style.backgroundColor = textColor;
+  textWrapper.style.backgroundColor = textColor || '#ffffff';
   const titleElement = document.createElement('h2');
   titleElement.textContent = title;
   const descriptionElement = document.createElement('p');
