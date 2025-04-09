51777a138235b4a65bd96c1e49f6b0fbd71299ad
JTorresV17 <juan.torres@omni.pro>
2025-04-09 12:45:17 -0500

    ajustes bloque de image

--- blocks/banco-image-position/banco-image-position.js
+++ blocks/banco-image-position/banco-image-position.js
@@ -1,14 +1,16 @@
 import { moveInstrumentation } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
-  block.innerHTML = '';
+  const items = Array.from(block.children);
 
-  const { 
-    positionImage, 
-    title, 
-    description, 
-    textColor 
-  } = block.dataset;
+  const imageElement = items.shift();
+  const textElement = items.shift();
+
+  const imgElement = imageElement?.querySelector('img');
+  const imgSrc = imgElement?.src || '';
+  const imgAlt = imgElement?.alt || 'Image';
+
+  const textContent = textElement?.textContent.trim() || '';
 
   const container = document.createElement('div');
   container.classList.add('image-text-container');
@@ -16,28 +18,20 @@ export default function decorate(block) {
   const imageWrapper = document.createElement('div');
   imageWrapper.classList.add('image-wrapper');
   const image = document.createElement('img');
-  image.src = positionImage;
-  image.alt = 'Imagen del bloque';
+  image.src = imgSrc;
+  image.alt = imgAlt;
   imageWrapper.appendChild(image);
-
-  moveInstrumentation(imageWrapper, image);
 
   const textWrapper = document.createElement('div');
   textWrapper.classList.add('text-wrapper');
-  textWrapper.style.backgroundColor = textColor || '#ffffff';
-  const titleElement = document.createElement('h2');
-  titleElement.textContent = title;
-  const descriptionElement = document.createElement('p');
-  descriptionElement.textContent = description;
+  textWrapper.textContent = textContent;
 
-  textWrapper.appendChild(titleElement);
-  textWrapper.appendChild(descriptionElement);
-
-  moveInstrumentation(textWrapper, titleElement);
-  moveInstrumentation(textWrapper, descriptionElement);
+  moveInstrumentation(imageElement, imageWrapper);
+  moveInstrumentation(textElement, textWrapper);
 
   container.appendChild(imageWrapper);
   container.appendChild(textWrapper);
 
+  block.innerHTML = '';
   block.appendChild(container);
 }
