fca8d616a0d883e9f2685d30c0bc45579cc05ec3
jeremyjbo98 <jeremy.ballesteros@omni.pro>
2025-04-09 16:40:25 -0500

    avance bloque slider

--- blocks/banco-compartir-carousel/banco-compartir-carousel.js
+++ blocks/banco-compartir-carousel/banco-compartir-carousel.js
@@ -8,36 +8,4 @@ export default function decorate(block) {
     [...row.children][0].className = 'banco-compartir-carousel-item-image';
     [...row.children][1].className = 'banco-compartir-carousel-item-title';
   });
-
-  const script1 = document.createElement('script');
-  script1.type = 'text/javascript';
-  script1.src = '//code.jquery.com/jquery-1.11.0.min.js';
-
-  const script2 = document.createElement('script');
-  script2.type = 'text/javascript';
-  script2.src = '//code.jquery.com/jquery-migrate-1.2.1.min.js';
-
-  const script3 = document.createElement('script');
-  script3.type = 'text/javascript';
-  script3.src = 'slick/slick.min.js';
-
-  block.appendChild(script1);
-  block.appendChild(script2);
-  block.appendChild(script3);
-
-  script1.onload = () => {
-    script2.onload = () => {
-      script3.onload = () => {
-        // Initialize the carousel after the scripts are loaded
-        $(document).ready(() => {
-          $('.banco-compartir-carousel-block').slick({
-            infinite: true,
-            slidesToShow: 3,
-            slidesToScroll: 3
-          });
-        });
-      };
-    };
-  };
-
 }
