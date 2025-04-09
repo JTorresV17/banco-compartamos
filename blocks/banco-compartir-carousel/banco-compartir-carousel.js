920188d8167541c9d85115447868389ce1fdc213
jeremyjbo98 <jeremy.ballesteros@omni.pro>
2025-04-09 16:22:13 -0500

    avance bloque slider

--- blocks/banco-compartir-carousel/banco-compartir-carousel.js
+++ blocks/banco-compartir-carousel/banco-compartir-carousel.js
@@ -11,24 +11,32 @@ export default function decorate(block) {
 
   const script1 = document.createElement('script');
   script1.type = 'text/javascript';
-  script1.src = 'https://cdn.jsdelivr.net/npm/jquery';
+  script1.src = '//code.jquery.com/jquery-1.11.0.min.js';
 
   const script2 = document.createElement('script');
   script2.type = 'text/javascript';
-  script2.src = 'https://cdn.jsdelivr.net/npm/slick-carousel/slick/slick.min.js';
+  script2.src = '//code.jquery.com/jquery-migrate-1.2.1.min.js';
+
+  const script3 = document.createElement('script');
+  script3.type = 'text/javascript';
+  script3.src = 'slick/slick.min.js';
 
   block.appendChild(script1);
   block.appendChild(script2);
+  block.appendChild(script3);
 
   script1.onload = () => {
     script2.onload = () => {
-      $(document).ready(function () {
-        $('.banco-compartir-carousel-block').slick({
-          infinite: false,
-          slidesToShow: 3,
-          slidesToScroll: 1
+      script3.onload = () => {
+        // Initialize the carousel after the scripts are loaded
+        $(document).ready(() => {
+          $('.banco-compartir-carousel-block').slick({
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 3
+          });
         });
-      });
+      };
     };
   };
 
