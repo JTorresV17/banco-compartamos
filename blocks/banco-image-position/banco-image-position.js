d0b180c58007dafc8b34e3c5aa385f6881bd4f7a
JTorresV17 <juan.torres@omni.pro>
2025-04-09 12:25:27 -0500

    ajustes bloque de image

--- blocks/banco-image-position/banco-image-position.js
+++ blocks/banco-image-position/banco-image-position.js
@@ -9,26 +9,24 @@ export default function decorate(block) {
   container.classList.add('image-text-container');
 
   // Obtenemos los valores del modelo JSON
-  const positionImage = block.dataset.positionImage; // Aquí obtienes la URL de la imagen seleccionada
-  const title = block.dataset.title; // El título del bloque
-  const description = block.dataset.description; // La descripción
-  const textColor = block.dataset.textColor || '#ffffff'; // El color de fondo, con un valor por defecto
+  const positionImage = block.dataset.positionImage;
+  const title = block.dataset.title;
+  const description = block.dataset.description;
+  const textColor = block.dataset.textColor || '#ffffff';
 
-  // Creamos el bloque de la imagen
   const imageWrapper = document.createElement('div');
   imageWrapper.classList.add('image-wrapper');
   const image = document.createElement('img');
-  image.src = positionImage; // Asignamos la imagen
+  image.src = positionImage;
   image.alt = 'Imagen del bloque';
   imageWrapper.appendChild(image);
 
-  // Usamos moveInstrumentation para mover la imagen
   moveInstrumentation(imageWrapper, image);
 
   // Creamos el bloque de texto
   const textWrapper = document.createElement('div');
   textWrapper.classList.add('text-wrapper');
-  textWrapper.style.backgroundColor = textColor; // Asignamos el color de fondo del texto
+  textWrapper.style.backgroundColor = textColor;
   const titleElement = document.createElement('h2');
   titleElement.textContent = title;
   const descriptionElement = document.createElement('p');
