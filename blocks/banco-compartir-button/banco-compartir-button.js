855d3c75f5c1a164467ba749ae976637599f693b
jeremyjbo98 <jeremy.ballesteros@omni.pro>
2025-04-10 16:05:13 -0500

    avance bloque slider

--- blocks/banco-compartir-button/banco-compartir-button.js
+++ blocks/banco-compartir-button/banco-compartir-button.js
@@ -3,23 +3,12 @@ export default function decorate(block) {
 
   block.className = 'banco-compartir-button-block';
 
-  const urlContainer = [...block.children].firstElementChild;
-  const contentContainer = [...block.children].lastElementChild;
+  const url = [...block.children].firstElementChild;
+  const content = [...block.children].lastElementChild;
 
-  const url = urlContainer.textContent;
-  const content = contentContainer.textContent;
-  
-  console.log('Button content:', content, 'URL:', url);
-
-  block.removeChild(urlContainer);
-  block.removeChild(contentContainer);
-  
-  const button = document.createElement('div');
-  button.className = 'banco-compartir-button-div';
-  button.textContent = content;
-
-  button.addEventListener('click', () => {
-    window.location.href = url;
-  });
+  console.log({
+    url: url,
+    content: content,
+  })
 
 }
