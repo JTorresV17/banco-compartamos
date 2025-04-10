5ef740668745d639055d540027cd7fd7816f2dcd
JTorresV17 <juan.torres@omni.pro>
2025-04-10 10:01:54 -0500

    ajustes bloque image

--- blocks/banco-image-position/banco-image-position.js
+++ blocks/banco-image-position/banco-image-position.js
@@ -1,15 +1,24 @@
 export default function decorate(block) {
     const container = document.createElement('div');
     container.className = 'image-text-container';
+
     const imageWrapper = document.createElement('div');
     imageWrapper.className = 'image-wrapper';
+
     const textContainer = document.createElement('div');
     textContainer.className = 'text-container';
+
     const titleWrapper = document.createElement('div');
     titleWrapper.className = 'title-wrapper';
+
     const descriptionWrapper = document.createElement('div');
     descriptionWrapper.className = 'description-wrapper';
+
     const children = [...block.children];
+
+    // Verifica si estamos en modo autor (AEM)
+    const isAuthorMode = window?.location?.hostname.includes('author');
+
     if (children[0]) {
         const image = children[0].querySelector('img');
         if (image) {
@@ -20,12 +29,16 @@ export default function decorate(block) {
 
     if (children[1]) {
         titleWrapper.innerHTML = children[1].innerHTML;
-        titleWrapper.setAttribute('contenteditable', 'true');
+        if (isAuthorMode) {
+            titleWrapper.setAttribute('contenteditable', 'true');
+        }
     }
 
     if (children[2]) {
         descriptionWrapper.innerHTML = children[2].innerHTML;
-        descriptionWrapper.setAttribute('contenteditable', 'true');
+        if (isAuthorMode) {
+            descriptionWrapper.setAttribute('contenteditable', 'true');
+        }
     }
 
     let cambiarOrden = false;
@@ -39,7 +52,6 @@ export default function decorate(block) {
     textContainer.appendChild(titleWrapper);
     textContainer.appendChild(descriptionWrapper);
 
-    // Orden condicional
     if (cambiarOrden) {
         container.appendChild(textContainer);
         container.appendChild(imageWrapper);
