8b4ba8cf22cb25d455472c8e329c691a5e41d860
JTorresV17 <juan.torres@omni.pro>
2025-04-10 15:49:58 -0500

    ajustes bloque section-social

--- blocks/banco-compartamos-section-social/banco-compartamos-section-social.js
+++ blocks/banco-compartamos-section-social/banco-compartamos-section-social.js
@@ -1,5 +1,5 @@
 export default function decorate(block) {
-    // Crear el contenedor principal
+    console.log('Decorating block categories', block);
     const container = document.createElement('div');
     container.className = 'banco-compartir-ui_section';
 
